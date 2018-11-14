0428078232f7c1f65817f7def63a925a830c680c
diego fontecilla <mail@diegofontecilla.com>
2018-11-14 14:26:59 +0000

    Created DataProcessor class. #most_founded_item returns the most founded item on the purchases json object

---
+++ lib/data_processor.rb
@@ -0,0 +1,29 @@
+class DataProcessor
+
+  def initialize(purchases, users = nil)
+    @purchases = purchases
+    @users = users
+  end
+
+  def most_founded_item
+    most_founded = []
+    count_elements.each {|id, number| most_founded << number}
+    count_elements.key(most_founded.max)
+  end
+
+  private
+
+  def get_elements
+    elements_array = []
+    @users == nil ? key = :item : key = :user_id
+    @purchases[:data].each {|purchase| elements_array << purchase[key]}
+    elements_array
+  end
+
+  def count_elements
+    elements_array = get_elements
+    elements_counted = {}
+    elements_array.uniq.each {|id| elements_counted[id] = elements_array.count(id)}
+    elements_counted
+  end
+end
